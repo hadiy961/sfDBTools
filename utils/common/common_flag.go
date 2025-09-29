f3495ba645435c4361b1b69137f452a1409c39c8
hadiyatna <hadiy961@gmail.com>
2025-09-29 14:36:19 +0700

    feat: remove deprecated ParseRestoreOptionsFromFlags function and clean up imports

--- utils/common/common_flag.go
+++ utils/common/common_flag.go
@@ -1,46 +1,12 @@
 package common
 
 import (
-	"fmt"
 	"os"
 	"strconv"
 	"strings"
 
-	"sfDBTools/internal/config"
-	RestoreUtils "sfDBTools/internal/core/restore/utils"
-
 	"github.com/spf13/cobra"
 )
-
-// ParseRestoreOptionsFromFlags parses restore options from command flags.
-// Deprecated: Use ResolveRestoreConfig from utils/restore package instead
-func ParseRestoreOptionsFromFlags(cmd *cobra.Command) (RestoreUtils.RestoreOptions, error) {
-	target_host, target_port, target_user, target_password,
-		_, _, _, _,
-		_, _, _, _, _ := config.GetBackupDefaults()
-
-	dbName := GetStringFlagOrEnv(cmd, "target_db", "TARGET_DB", "")
-	if dbName == "" {
-		return RestoreUtils.RestoreOptions{}, fmt.Errorf("database name is required")
-	}
-
-	host := GetStringFlagOrEnv(cmd, "target_host", "TARGET_HOST", target_host)
-	port := GetIntFlagOrEnv(cmd, "target_port", "TARGET_PORT", target_port)
-	user := GetStringFlagOrEnv(cmd, "target_user", "TARGET_USER", target_user)
-	password := GetStringFlagOrEnv(cmd, "target_password", "TARGET_PASSWORD", target_password)
-	file := GetStringFlagOrEnv(cmd, "file", "FILE", "")
-	VerifyChecksum := GetBoolFlagOrEnv(cmd, "verify-checksum", "VERIFY_CHECKSUM", false)
-
-	return RestoreUtils.RestoreOptions{
-		Host:           host,
-		Port:           port,
-		User:           user,
-		Password:       password,
-		DBName:         dbName,
-		File:           file,
-		VerifyChecksum: VerifyChecksum,
-	}, nil
-}
 
 func GetStringFlagOrEnv(cmd *cobra.Command, flagName, envName string, defaultVal string) string {
 	val, _ := cmd.Flags().GetString(flagName)
